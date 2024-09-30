c1da3c9eeb612afa08d58b2a654d51d6e4738bdb
HD-MOE <tok_hong_da@moe.edu.sg>
2024-09-30 17:54:49 +0800

    First Commit

---
+++ Example1.py
@@ -0,0 +1,13 @@
+# 2.2.1 Show a temporary message with a button
+
+import streamlit as st 
+
+#Example: Show a temporary message with a button
+
+animal_shelter = ['cat', 'dog', 'rabbit', 'bird'] 
+
+animal = st.text_input('Type an animal') 
+
+if st.button('Check availability'): 
+	have_it = animal.lower() in animal_shelter 
+	'We have that animal!' if have_it else 'We don\'t have that animal.'
